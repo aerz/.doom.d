877cda2f027ef88bab393c1a2a0ef73663298b3b
Agustín Cisneros <agustincc@tutanota.com>
2026-06-25 11:52:51 +0200

    fix prettier-jinja2 formatter definition
    * replace mode-only association with explicit formatter args pointing
      to prettier with the jinja-template parser: https://github.com/davidodenwald/prettier-plugin-jinja-template
    
      with .dir-locals:
      ```
      ;;; Directory Local Variables -*- no-byte-compile: t -*-
    
      ((jinja2-mode . ((apheleia-formatter . prettier-jinja2))))
      ```
    
    * add .gitignore for .DS_Store, *.log, tmp/, and custom.el.

--- modules/lang/jinja2.el
+++ modules/lang/jinja2.el
@@ -1,4 +1,8 @@
 ;;; langs/jinja2.el -*- lexical-binding: t; -*-
 
-(after! jinja2-mode
-  (set-formatter! 'prettier-jinja2 :modes '(jinja2-mode)))
+(after! apheleia
+  (set-formatter!
+    'prettier-jinja2
+    '("prettier"
+      "--parser=jinja-template"
+      "--stdin-filepath" filepath)))
